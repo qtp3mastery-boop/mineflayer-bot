a6e91e4bbc3f8ee5e9840f451ff0d8c22314f3b1
qtp3mastery-boop <qtp3mastery@gmail.com>
2026-06-01 14:31:31 +0500

    Initialize bot with mineflayer and spawn event

--- bot.js
+++ bot.js
@@ -1,1 +1,11 @@
+const mineflayer = require('mineflayer')
 
+const bot = mineflayer.createBot({
+  host: 'YOUR_SERVER_IP',
+  port: 25565,
+  username: 'BotName'
+})
+
+bot.on('spawn', () => {
+  console.log('Bot joined!')
+})
